e94e9b17b4124dd1c4fbbef6e53812a6e9398760
christopherjacksonjr <jacksonchristopher0924@gmail.com>
2019-09-09 22:33:01 +0000

    feature - user is able to delete campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -93,6 +93,17 @@ router.put('/:id', function(req, res) {
 	});
 });
 
+//DESTROY CAMGROUND ROUTE
+router.delete('/:id', function(req, res) {
+	Campground.findByIdAndRemove(req.params.id, function(err) {
+		if(err) {
+			res.redirect('/campgrounds');
+		} else {
+			res.redirect('/campgrounds');
+		}	 
+	});
+});
+
 //Middleware
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()){

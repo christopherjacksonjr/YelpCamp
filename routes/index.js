68a2600079d9d092bd87129632c9449da3b7a6ee
christopherjacksonjr <jacksonchristopher0924@gmail.com>
2019-09-17 03:35:57 +0000

    adding Bootstrap to flash messages

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.post('/register', function(req, res) {
 
 //Show login form
 router.get('/login', function(req, res) {
-	res.render('login', {message: req.flash('error')});
+	res.render('login');
 });
 
 //Handle login logic
@@ -48,6 +48,7 @@ router.post('/login', passport.authenticate('local',
 //Logout Route
 router.get('/logout', function(req, res) {
 	req.logout();
+	req.flash('success', 'Logged you out!');
 	res.redirect('/campgrounds');
 })
 
